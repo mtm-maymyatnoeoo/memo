3dfd10aaecc8bbea66f54ab3ce727eb0168a6f70
MTM-A Mi Mi Htun <scm.amimitun@gmail.com>
2026-09-18 16:49:33 +0630

    change client

--- js/supabase.js
+++ js/supabase.js
@@ -1,5 +1,5 @@
-// import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+// import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 const SUPABASE_URL = "https://psgyanmneunqifqmvzkr.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_5fFTQWGDwnQTcOi95lzVzg_iOalzs5V";
 
